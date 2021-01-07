a3550bdbe2725ee6cfcafc74e7deb7cde50a37cf
Edison_ForMan <chuanghewzh12@hotmail.com>
2021-01-07 13:04:10 +0800

    update

--- src/api/videoAPI.js
+++ src/api/videoAPI.js
@@ -1,5 +1,15 @@
+/*
+ * @Author: eds
+ * @Date: 2021-01-07 13:03:16
+ * @LastEditTime: 2021-01-07 13:03:57
+ * @LastEditors: eds
+ * @Description:
+ * @FilePath: \tool-videoToScene\src\api\videoAPI.js
+ */
 import axios from "axios";
-const BASEURL = "http://localhost:3000";
+const BASEURL = ~window.location.host.indexOf("localhost")
+  ? "http://localhost:3000"
+  : "http://10.36.198.161:3000";
 const serverInstanec = axios.create();
 serverInstanec.defaults.baseURL = BASEURL;
 
@@ -9,16 +19,16 @@ serverInstanec.defaults.baseURL = BASEURL;
  * @param {*} data
  */
 const getAxios = (url = "", params = {}, method = "get") => {
-    const option = { url, method };
-    method == 'get' ? option.params = params : option.data = params;
-    return serverInstanec.request(option).then(res => {
-        return res.data ? Promise.resolve(res.data.data) : Promise.reject(res);
-    });
+  const option = { url, method };
+  method == "get" ? (option.params = params) : (option.data = params);
+  return serverInstanec.request(option).then(res => {
+    return res.data ? Promise.resolve(res.data.data) : Promise.reject(res);
+  });
 };
 
-export const getVideoProjections = (id) => {
-    return getAxios("/video/getVideoProjections", { id })
-}
+export const getVideoProjections = id => {
+  return getAxios("/video/getVideoProjections", { id });
+};
 export const saveVideoProjections = (id, params) => {
-    return getAxios("/video/saveVideoProjections", { id, params }, 'post')
-}
+  return getAxios("/video/saveVideoProjections", { id, params }, "post");
+};
